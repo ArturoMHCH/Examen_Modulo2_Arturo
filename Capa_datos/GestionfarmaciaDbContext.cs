4033a4fcde08d600cfd6b86924e3d3e2a12fc1c9
ArturoMHCH <mscaljrokline@gmail.com>
2022-11-17 15:42:18 -0600

    Agregando capa de datos y el modelo farmaceutico, ademas de gestionfarmacia y appsettings con cadena de conexion

---
+++ Capa_datos/GestionfarmaciaDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Capa_datos.Modelos;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Capa_datos
+{
+    public class GestionfarmaciaDbContext : DbContext
+    {
+        public DbSet<farmaceutico> Farmaceuticos { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder option)
+        {
+            if (!option.IsConfigured)
+            {
+                var builder = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json");
+                var config = builder.Build();
+                option.UseSqlServer(config["ConnectionStrings:miConexion"]);
+            }
+        }
+
+    }
+}
---
+++ Capa_datos/Modelos/farmaceutico.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Capa_datos.Modelos
+{
+    [Table("farmaceutico")]
+
+    public class farmaceutico
+    {
+        [Key]
+        public int ci { get; set; }
+        public string nombreFarm { get; set; }
+        public string apellidoFarm { get; set; }
+        public DateTime fecha_nac { get; set; }
+        public string contrasena { get; set; }
+        public char cargo { get; set; }
+
+    }
+}
